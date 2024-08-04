3ddb946b6fc98545060c798a8fc2e178431c2ef4
Oleksandr Pavlenko <oleksandr.pavlenko.op@gmail.com>
2024-08-04 22:11:43 +0200

    add MinimumNumberOfMovesToSeatEveryone

---
+++ src/main/java/com/algorithmen/sort/MinimumNumberOfMovesToSeatEveryone.java
@@ -0,0 +1,33 @@
+package com.algorithmen.sort;
+
+import java.util.Arrays;
+
+/*There are n availabe seats and n students standing in a room. You are given an array seats of length n, where seats[i] is the position of the ith seat. You are also given the array students of length n, where students[j] is the position of the jth student.
+
+You may perform the following move any number of times:
+
+Increase or decrease the position of the ith student by 1 (i.e., moving the ith student from position x to x + 1 or x - 1)
+Return the minimum number of moves required to move each student to a seat such that no two students are in the same seat.
+
+Note that there may be multiple seats or students in the same position at the beginning.*/
+
+//runtime beats 90%
+public class MinimumNumberOfMovesToSeatEveryone {
+    public int minMovesToSeat(int[] seats, int[] students) {
+        if (seats.length == 0 || students.length == 0 || seats.length != students.length) {
+            return 0;
+        }
+
+        int moves = 0;
+
+        Arrays.sort(seats);
+        Arrays.sort(students);
+
+        for (int i = 0; i < seats.length; i++) {
+            moves += Math.abs(students[i] - seats[i]);
+
+        }
+        return moves;
+    }
+}
+
