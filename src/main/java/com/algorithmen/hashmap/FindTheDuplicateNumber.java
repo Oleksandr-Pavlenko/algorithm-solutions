3e42b801ef97ba7cede7432d8e852638ae0a7b36
Oleksandr Pavlenko <oleksandr.pavlenko.op@gmail.com>
2024-09-24 10:57:52 +0200

    add FindTheDuplicateNumber

---
+++ src/main/java/com/algorithmen/hashmap/FindTheDuplicateNumber.java
@@ -0,0 +1,22 @@
+package com.algorithmen.hashmap;
+
+import java.util.*;
+
+/*Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
+
+There is only one repeated number in nums, return this repeated number.
+
+You must solve the problem without modifying the array nums and using only constant extra space.*/
+public class FindTheDuplicateNumber {
+    //runtime 34%, memory 93%
+    public int findDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            if (set.contains(num)) {
+                return num;
+            }
+            set.add(num);
+        }
+        return 0;
+    }
+}
