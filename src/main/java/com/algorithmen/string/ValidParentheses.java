3629e82cebf958ebf13cd6be4c29ec2942944d03
Oleksandr Pavlenko <oleksandr.pavlenko.op@gmail.com>
2024-09-30 22:29:16 +0200

    add ValidParentheses

---
+++ src/main/java/com/algorithmen/string/ValidParentheses.java
@@ -0,0 +1,56 @@
+package com.algorithmen.string;
+
+import java.util.Map;
+import java.util.Stack;
+
+//Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+//
+//An input string is valid if:
+//
+//Open brackets must be closed by the same type of brackets.
+//Open brackets must be closed in the correct order.
+//Every close bracket has a corresponding open bracket of the same type
+public class ValidParentheses {
+    // Approach 1
+    // Runtime beats 12%, memory 84%
+    public boolean isValid1(String s) {
+        Map<Character, Character> map = Map.of(
+                '(', ')',
+                '{', '}',
+                '[', ']');
+
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (map.containsKey(c)) {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                char open = stack.pop();
+                if (map.get(open) != c) {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    // Approach 2
+    // Runtime beats 99 %, memory 37%
+    public boolean isValid2(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(')
+                stack.push(')');
+            else if (c == '{')
+                stack.push('}');
+            else if (c == '[')
+                stack.push(']');
+            else if (stack.isEmpty() || stack.pop() != c)
+                return false;
+        }
+
+        return stack.isEmpty();
+    }
+}
